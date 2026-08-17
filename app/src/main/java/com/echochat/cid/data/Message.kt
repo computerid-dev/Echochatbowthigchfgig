2443fb8263d95d7bf609af82ebdb836ea5d803e1
computerid-dev <nugrohokelyn@gmail.com>
2026-08-17 15:41:35 +0700

    Upload app/src/main/java/com/echochat/cid/data/Message.kt

---
+++ app/src/main/java/com/echochat/cid/data/Message.kt
@@ -0,0 +1,24 @@
+package com.echochat.cid.data
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "messages",
+    indices = [
+        Index(value = ["chatWithUid"]),
+        Index(value = ["remoteId"], unique = true)
+    ]
+)
+data class Message(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val chatWithUid: String,
+    val content: String,
+    val imageBase64: String? = null,
+    val isMine: Boolean,
+    val remoteId: String,
+    val isRead: Boolean = true,
+    val timestamp: Long = System.currentTimeMillis()
+)
